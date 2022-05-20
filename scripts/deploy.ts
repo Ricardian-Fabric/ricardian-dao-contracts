27fe7abbffde72cbcd9eef96a86adb534ef8b5d6
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-05-20 22:15:31 +0100

    4 million OneRic will be minted

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,13 +4,8 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
-export const RICTOTALSUPPLY = "100000000"; // 100.000.000
-export const DAOSTAKINGALLOCATION = "20000000"; // 20.000.000
-export const MEMBERSHIPALLOCATION = "40000000"; // 40.000.000
-
-export const RICKVAULTALLOCATION = "40000000"; // TOTAL RIC ALLOCATION 20.000.000
-export const RICTOLOCK = "2000000"; // 2.000.000 Lock this amount for RICLOCKINTERVAL
-export const RICLOCKINTERVAL = 7000000; // 7.000.000
+// OneRIC will need to be 4 million
+export const RICTOTALSUPPLY = "4000000"; // 4.000.000
 
 export const FEEDAOPOLLPERIOD = 259200; // The front end is coded to use the same values for the periods!
 export const CATALOGPOLLPERIOD = 259200; // around 6 days
@@ -19,20 +14,8 @@ export const CATALOGPOLLPERIOD = 259200; // around 6 days
 export const RICSELLERADDRESS = "0xDF16399E6F10bbC1C07C88c6c70116182FA2e118";
 const DAOSTAKINGPERIOD = 864000; // around 20 days
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
   await deploymentScript({
     RICTOTALSUPPLY,
-    DAOSTAKINGALLOCATION,
-    MEMBERSHIPALLOCATION,
-    RICKVAULTALLOCATION,
-    RICTOLOCK,
-    RICLOCKINTERVAL,
     FEEDAOPOLLPERIOD,
     CATALOGPOLLPERIOD,
     DAOSTAKINGPERIOD,
@@ -41,11 +24,6 @@ async function main() {
 
 type DeploymentArg = {
   RICTOTALSUPPLY: string;
-  DAOSTAKINGALLOCATION: string;
-  MEMBERSHIPALLOCATION: string;
-  RICKVAULTALLOCATION: string;
-  RICTOLOCK: string;
-  RICLOCKINTERVAL: number;
   FEEDAOPOLLPERIOD: number;
   CATALOGPOLLPERIOD: number;
   DAOSTAKINGPERIOD: number;
@@ -68,10 +46,6 @@ export async function deploymentScript(arg: DeploymentArg) {
       " gasLimit: ",
       signup.deployTransaction.gasLimit
     );
-    // console.log(
-    //   "Signup is already deployed on mainnet to ",
-    //   "0xdC627A00D6d717c3A920ed07C28027E6f4474dF6"
-    // );
     setTimeout(async () => {
       const RicToken = await ethers.getContractFactory("Ric");
 
@@ -198,57 +172,6 @@ export async function deploymentScript(arg: DeploymentArg) {
                         await ricvault.setFeeDao(feedao.address, {
                           gasPrice: gasPrice,
                         });
-
-                        // setTimeout(async () => {
-                        //   await ric.approve(
-                        //     daoStaking.address,
-                        //     ethers.utils.parseEther(
-                        //       arg.DAOSTAKINGALLOCATION
-                        //     ),
-                        //     { gasPrice: gasPrice }
-                        //   );
-                        //   setTimeout(async () => {
-                        //     // 20 % goes to the daoStaking reward
-
-                        //     await daoStaking.depositRewards(
-                        //       ethers.utils.parseEther(
-                        //         arg.DAOSTAKINGALLOCATION
-                        //       ),
-                        //       { gasPrice: gasPrice }
-                        //     );
-                        //     setTimeout(async () => {
-                        //       // 40 % goes to the membership (crowdsale) contract
-                        //       // No transfer, only approval
-                        //       await ric.approve(
-                        //         ricsale.address,
-                        //         ethers.utils.parseEther(
-                        //           arg.MEMBERSHIPALLOCATION
-                        //         ),
-                        //         { gasPrice: gasPrice }
-                        //       );
-                        //       setTimeout(async () => {
-                        //         console.log(
-                        //           await ric.allowance(
-                        //             RICSELLERADDRESS,
-                        //             ricsale.address
-                        //           )
-                        //         );
-                        //         console.log(
-                        //           await ricsale.remainingTokens()
-                        //         );
-
-                        //         // 20 % goes to the Ric vault,
-                        //         await ric.approve(
-                        //           ricvault.address,
-                        //           ethers.utils.parseEther(
-                        //             arg.RICKVAULTALLOCATION
-                        //           ),
-                        //           { gasPrice: gasPrice }
-                        //         );
-                        //       }, 38000);
-                        //     }, 36000);
-                        //   }, 34000);
-                        // }, 32000);
                       }, 28000);
                     }, 26000);
                   }, 24000);
@@ -260,22 +183,6 @@ export async function deploymentScript(arg: DeploymentArg) {
       }, 8000);
     }, 6000);
   }, 4000);
-
-  // We get the contract to deploy
-
-  // Token allocation script!
-
-  // Locked with increasing lock up periods for RICTOLOCK amount of tokens.
-  // 1. RICTOLOCK is 2% of the total supply
-  // for (let i = 1; i <= 16; i++) {
-  //   await ricvault.lockFunds(
-  //     arg.RICLOCKINTERVAL * i,
-  //     ethers.utils.parseEther(arg.RICTOLOCK)
-  //   );
-  // }
-  // 20 % Ecosystem / Liquidity / 30 RIC Grants / wallet for making proposals / Airdrops
-
-  // Manual lockups!
 }
 
 // We recommend this pattern to be able to use async/await everywhere
